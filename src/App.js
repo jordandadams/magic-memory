7bcdb3a761282de63c670f38ef10d83ef3ab79a1
jordandadams <jordanadams61791@gmail.com>
2022-03-01 20:38:20 -0600

    first commit for mem game

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import './App.css'
+
+//creates cards array
+const cardImages = [
+  { "src": "/img/helmet-1.png" },
+  { "src": "/img/portion-1.png" },
+  { "src": "/img/ring-1.png" },
+  { "src": "/img/scroll-1.png" },
+  { "src": "/img/shield-1.png" },
+  { "src": "/img/sword-1.png" }
+]
+
+//component
+function App() {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+
+  //shuffle cards
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+    //sort images
+    .sort(() => Math.random() - 0.5)
+    .map((card) => ({ ...card, id: Math.random() }))
+
+    setCards(shuffleCards)
+    setTurns(0)
+  }
+
+  console.log(cards, turns)
+
+  return (
+    <div className="App">
+      <h1>Magic Match</h1>
+      <button onClick={shuffleCards}>New Game</button>
+    </div>
+  );
+}
+
+export default App
